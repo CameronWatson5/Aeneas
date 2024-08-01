83bc467a69cf65edd383b7e601a61c94987a952e
Cameron Watson <2392496w@student.gla.ac.uk>
2024-08-01 14:13:02 +0100

    Add text based opeing cut scene to give context to game.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,32 +5,65 @@ public class MainMenu : MonoBehaviour
 {
     public Button startButton;
     public Button quitButton;
+    public GameObject mainMenuElements; 
+    public GameObject cutscenePanel;
 
     private void Start()
     {
-        if (startButton != null) startButton.onClick.AddListener(StartGame);
-        else Debug.LogError("StartButton reference is missing.");
+        Debug.Log("MainMenu: Start called");
+        
+        if (startButton != null)
+        {
+            startButton.onClick.AddListener(StartGame);
+            Debug.Log("MainMenu: Start button listener added");
+        }
+        else Debug.LogError("MainMenu: StartButton reference is missing");
 
-        if (quitButton != null) quitButton.onClick.AddListener(QuitGame);
-        else Debug.LogError("QuitButton reference is missing.");
+        if (quitButton != null)
+        {
+            quitButton.onClick.AddListener(QuitGame);
+            Debug.Log("MainMenu: Quit button listener added");
+        }
+        else Debug.LogError("MainMenu: QuitButton reference is missing");
+
+        if (cutscenePanel != null)
+        {
+            cutscenePanel.SetActive(false);
+            Debug.Log("MainMenu: Cutscene panel set to inactive");
+        }
+        else Debug.LogError("MainMenu: CutscenePanel reference is missing");
     }
 
     public void StartGame()
     {
-        Debug.Log("Starting new game.");
-        if (SceneTransitionManager.Instance != null)
+        Debug.Log("MainMenu: StartGame called");
+        
+        if (mainMenuElements != null)
         {
-            SceneTransitionManager.Instance.StartNewGame("Troy", "SpawnPoint1");
+            mainMenuElements.SetActive(false);
+            Debug.Log("MainMenu: Main menu elements deactivated");
         }
-        else
+        else Debug.LogError("MainMenu: mainMenuElements reference is missing");
+
+        if (cutscenePanel != null)
         {
-            Debug.LogError("SceneTransitionManager instance is not set.");
+            cutscenePanel.SetActive(true);
+            Debug.Log("MainMenu: Cutscene panel activated");
+
+            CutsceneManager cutsceneManager = cutscenePanel.GetComponent<CutsceneManager>();
+            if (cutsceneManager != null)
+            {
+                cutsceneManager.enabled = true;
+                Debug.Log("MainMenu: CutsceneManager enabled");
+            }
+            else Debug.LogError("MainMenu: CutsceneManager component is missing on the CutscenePanel");
         }
+        else Debug.LogError("MainMenu: CutscenePanel reference is missing");
     }
 
     public void QuitGame()
     {
-        Debug.Log("Quitting game.");
+        Debug.Log("MainMenu: QuitGame called");
         Application.Quit();
     }
 }
